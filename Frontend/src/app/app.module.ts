183fa6f6a0be98be0178267261217798376ebdaf
sajidbaloch1 <kinglovers109@gmail.com>
2023-09-27 15:59:25 +0500

    login layout

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import {  CheckboxModule } from 'primeng/checkbox';
+import {  MenubarModule } from 'primeng/menubar';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    CheckboxModule
+    ,MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
